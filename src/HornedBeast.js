f5048d7496dddf4b4884761744ea68b1fafcf257
Alec Torres <atorresla24@gmail.com>
2022-06-15 18:54:45 -0400

    emoji

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -22,8 +22,8 @@ class HornedBeast extends React.Component{
       <article>
         <id>{this.props._id}</id>
         <h2>{this.props.title}</h2>
-        <p>{this.state.likes} Likes</p>
-        <p onClick={this.handleLikes}>💖</p>
+        <p>{this.state.likes} Likes </p>
+        <p onClick={this.handleLikes}> 💖</p>
         <img src={this.props.image_url} alt={this.props.keyword} title={this.props.title}/>
         <p>{this.props.description}</p>
         <Button>Bootstrap button</Button>
